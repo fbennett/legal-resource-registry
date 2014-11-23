52c9f788c709708c0bf3bdbdb015700166aa1fbd
Frank Bennett <biercenator@gmail.com>
2014-11-23 16:29:58 +0900

    Remove superceded build script

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./tools/grind-html-pages.py --stylesheet=legalcitem.css --link-stylesheet FederalCourts.rst > public/index.html
-./tools/grind-html-pages.py --stylesheet=legalcitem.css --link-stylesheet StateCourts.rst > public/states.html
-git commit -m "Updating site" public
-git subtree push --prefix=public git@github.com:jqheywood/CourtsSC.git gh-pages
