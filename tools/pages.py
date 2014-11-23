67444dfeee3517eb9c7a7b00e2f90f6df8ec2802
Frank Bennett <biercenator@gmail.com>
2014-11-23 16:32:10 +0900

    Update build scripts

---
+++ tools/pages.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+
+import re,sys,os,os.path
+
+from docutils.core import publish_string
+from docutils.parsers.rst import directives
+from rst4legalResourceRegistry import WriterForLegalCitem, JurisdictionDirective, FieldsDirective, CourtDirective, CitationGroupDirective, ReporterDirective, NotesDirective, FEATURES
+
+directives.register_directive('jurisdiction', JurisdictionDirective)
+directives.register_directive('fields', FieldsDirective)
+directives.register_directive('court', CourtDirective)
+directives.register_directive('citation-group', CitationGroupDirective)
+directives.register_directive('reporter', ReporterDirective)
+directives.register_directive('notes', NotesDirective)
+
+pth = os.path.split(sys.argv[0])[0]
+pth = os.path.join(pth,"..")
+pth = os.path.abspath(pth)
+
+class IndentSyntaxException:
+    def __init__(self,lineno,offset):
+        self.lineno = lineno
+        self.offset = offset
+
+class GeneralSyntaxException:
+    def __init__(self,lineno):
+        self.lineno = lineno
+
+class LineInfo:
+    def __init__(self,lineno,line):
+        bubblePageRex = re.compile("^(\s*)([^;<]+)<([^ ]+)>$")
+        detailsPageRex = re.compile("^(\s*)([^ ;]+;[^ ]+)\s*$")
+        courtTitleRex = re.compile(".. court::\s+(.*)$")
+        self.title = None
+        self.page_name = None
+        self.line_type = None
+        self.level = None
+
+        m = bubblePageRex.match(line)
+        if m:
+            offset = len(m.group(1))
+            self.line_type = "bubblePage"
+            self.title = m.group(2)
+            self.page_name = m.group(3)
+        if not m:
+            m = detailsPageRex.match(line)
+            if m:
+                offset = len(m.group(1))
+                self.line_type = "detailsPage"
+                self.page_name = m.group(2)
+                mypath = os.path.join(*[pth, "data","courts"] + self.page_name.split(";") + ["index.txt"])
+                ifh = open(mypath)
+                while 1:
+                    line = ifh.readline()
+                    if not line: break
+                    m = courtTitleRex.match(line)
+                    if m:
+                        self.title = m.group(1).rstrip()
+                        break
+                ifh.close()
+
+        if self.line_type:
+            if offset % 4:
+                raise IndentSyntaxException(lineno,offset)
+            self.level = offset/4
+                
+class jurisdictionStack:
+    def __init__(self):
+        self.stack = []
+        self.parent = None
+        self.current = None
+
+    def setLevelForPage(self,level,page_name):
+        self.stack = self.stack[0:level]
+        self.parent = os.path.join(*self.stack + ["index.html"])
+        pos = page_name.rfind(";")
+        if pos > -1:
+            page_name = page_name[pos+1:]
+        self.stack.append(page_name)
+        self.current = os.path.join(*self.stack + ["index.html"])
+        self.current_url = os.path.join(*self.stack[len(self.stack)-1:len(self.stack)] + ["index.html"])
+        self.backtrack_path = os.path.join(*[".."] * len(self.stack))
+
+class PageSource:
+    def __init__(self):
+        self.title = None
+        self.rst = ""
+
+    def setTopmatter(self):
+        self.rst += ".. include:: %s\n\n" % os.path.join(pth,"doc-src","fields.rst")
+        self.rst += ".. include:: %s\n\n" % os.path.join(pth,"doc-src","banner.rst")
+
+    def setTitle(self,title,char="-"):
+        self.title = title
+        line = char * len(title)
+        self.rst += "%s\n%s\n%s\n" % (line,title,line)
+
+    def setDraft(self):
+        self.rst += "\n.. include:: %s\n" % os.path.join(pth,"doc-src","draft-note.rst")
+        
+    def setCredits(self):
+        self.rst += "\n.. include:: %s\n" % os.path.join(pth,"doc-src","credits.rst")
+        
+    def setBackref(self,backtrack,url):
+        
+        self.rst += "\n.. image:: %s/graphics/circle-arrow-down.png\n   :target: %s\n   :class: back-button\n\n" % (backtrack,url)
+
+    def setBubbles(self):
+        self.rst += "\n.. container:: bubbles\n\n"
+
+    def addBubble(self,title,current):
+        self.rst += "   `%s <%s>`_\n" % (title,current)
+
+    def setJurisdiction(self,page_name):
+        self.rst += "\n.. jurisdiction:: %s" % page_name
+
+
+class PageEngine:
+    def __init__(self):
+        specpth = os.path.join(pth,"doc-src","pages.txt")
+        self.input = open(specpth)
+        self.source = {}
+
+    def getSpec(self):
+        self.stack = jurisdictionStack()
+        lineno = 0
+        while 1:
+            line = self.input.readline()
+            if not line: break
+            line = line.rstrip()
+            lineno += 1
+            lineInfo = LineInfo(lineno,line)
+            if lineInfo.line_type == "bubblePage":
+                self.stack.setLevelForPage(lineInfo.level,lineInfo.page_name)
+                if not self.source.has_key(self.stack.current):
+                    self.source[self.stack.current] = PageSource()
+                    self.source[self.stack.current].setTopmatter()
+                    self.source[self.stack.current].setTitle(lineInfo.title)
+                    self.source[self.stack.current].setDraft()
+                    self.source[self.stack.current].setCredits()
+                    self.source[self.stack.current].setBackref(self.stack.backtrack_path,"../index.html")
+                    self.source[self.stack.current].setBubbles()
+                if lineInfo.level:
+                    self.source[self.stack.parent].addBubble(lineInfo.title,self.stack.current_url)
+            elif lineInfo.line_type == "detailsPage":
+                self.stack.setLevelForPage(lineInfo.level,lineInfo.page_name)
+                self.source[self.stack.parent].addBubble(lineInfo.title,self.stack.current_url)
+                if not self.source.has_key(self.stack.current):
+                    self.source[self.stack.current] = PageSource()
+                    self.source[self.stack.current].setTopmatter()
+                    self.source[self.stack.current].setTitle(self.source[self.stack.parent].title)
+                    self.source[self.stack.current].setDraft()
+                    self.source[self.stack.current].setCredits()
+                    self.source[self.stack.current].setBackref(self.stack.backtrack_path,"../index.html")
+                    self.source[self.stack.current].setJurisdiction(lineInfo.page_name)
+            elif not line:
+                continue
+            else:
+                raise GeneralSyntaxException(lineno)
+        self.input.close()
+
+    def publishPage(self,key,src):
+        pubpath = os.path.join(pth,"public",key)
+        try:
+            os.makedirs(os.path.split(pubpath)[0])
+        except:
+            pass
+        FEATURES.local = None
+        writer = WriterForLegalCitem()
+        keylst = key.split("/")
+        upset = [".."] * (len(keylst)-1)
+        upset.append("legalcitem.css")
+        css = os.path.sep.join(upset)
+        
+        options = {
+            "stylesheet": css,
+            "stylesheet_path": None,
+            "embed_stylesheet": False
+            }
+        html = publish_string(src, reader=None, reader_name="standalone", writer=writer, settings_overrides=options)
+        open(pubpath,"w+").write(html)
+        
+
+    def dumpPages(self):
+        indexpath = os.path.join(pth,"doc-src","index.rst")
+        src = open(indexpath).read()
+        pwd = os.getcwd()
+        os.chdir(os.path.join(pth,"doc-src"))
+        self.publishPage("index.html",src)
+        os.chdir(pwd)
+        for key in self.source.keys():
+            print key
+            self.publishPage(key,self.source[key].rst)
+
+pageEngine = PageEngine()
+try:
+    pageEngine.getSpec()
+except IndentSyntaxException as err:
+    print "Error in pages.txt: offset of %d at line number %d is not a multiple of four." % (err.offset,err.lineno)
+except GeneralSyntaxException as err:
+    print "Error in pages.txt: unable to parse entry at line %d." % (err.lineno)
+
+pageEngine.dumpPages()
