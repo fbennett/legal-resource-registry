732708a44c0553ede3e78154764a735b2c3dc345
Frank Bennett <biercenator@gmail.com>
2014-12-03 19:02:14 +0900

    Update reporters-db branch from subtree

--- publish.sh
+++ publish.sh
@@ -3,3 +3,5 @@
 ./tools/pages.py
 git commit -m "Updating site" public
 git subtree push --prefix=public git@github.com:fbennett/legal-resource-registry.git gh-pages
+git commit -m "Updating site" reporters-db
+git subtree push --prefix=reporters-db git@github.com:fbennett/legal-resource-registry.git reporters-db
