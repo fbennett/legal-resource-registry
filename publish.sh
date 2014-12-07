69b3116389a5761665d0a3edfe88363f6ad4992c
Frank Bennett <biercenator@gmail.com>
2014-12-08 08:50:39 +0900

    Push only in publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-./tools/pages.py
-
 git commit -m "Updating site" public
 git subtree push --prefix=public git@github.com:fbennett/legal-resource-registry.git gh-pages
-
-git commit -m "Updating site" reporters-db
-git subtree push --prefix=reporters-db git@github.com:fbennett/legal-resource-registry.git reporters-db
