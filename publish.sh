01d49bae70a4cabb65622f64d0772ef18ace8dbc
Frank Bennett <biercenator@gmail.com>
2019-08-22 06:01:03 +0900

    Remove automation from gh-pages push

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp doc-src/screen.css public
-cp -r doc-src/bower_components public/
-cp -r doc-src/graphics public/
-cp -r doc-src/proofs-by-excel public/
+#cp doc-src/screen.css public
+#cp -r doc-src/bower_components public/
+#cp -r doc-src/graphics public/
+#cp -r doc-src/proofs-by-excel public/
 
 #git add public
-git commit -m "Updating site" public
+git commit -m "Updating site from ./public" public
 #git subtree add --prefix public git@github.com:fbennett/legal-resource-registry.git gh-pages --squash
 git subtree push --prefix=public git@github.com:fbennett/legal-resource-registry.git gh-pages
